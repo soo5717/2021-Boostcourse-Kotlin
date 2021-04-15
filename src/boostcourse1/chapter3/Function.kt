cc56e27a564bd3bb14e5e664cc42348d23b0d5a7
soo5717 <koo5717@naver.com>
2021-04-15 23:14:20 +0900

    코틀린 프로그래밍 기본1 - 2단원

---
+++ src/boostcourse1/chapter3/Function.kt
@@ -0,0 +1,8 @@
+package boostcourse1.chapter3
+
+fun sum(a: Int, b: Int): Int{
+    var sum = a + b
+    return sum
+
+//    return a+b //(간략화 버전)
+}
---
+++ src/boostcourse1/chapter3/Lamda.kt
@@ -0,0 +1,7 @@
+package boostcourse1.chapter3
+
+fun main() {
+    println(highFunc({ x, y -> x + y }, 10, 20)) // 람다식 함수를 인자로 넘김
+}
+
+fun highFunc(sum: (Int, Int) -> Int, a: Int, b: Int): Int = sum(a, b) // sum 매개변수는 함수
