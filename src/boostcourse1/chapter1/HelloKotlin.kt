4a2ba3f07a6c50a0904bdb4dc3ea7738d4a5bdbc
soo5717 <koo5717@naver.com>
2021-04-23 21:29:04 +0900

    코틀린 프로그래밍 기본1 - 챕터1

--- src/boostcourse1/chapter1/HelloKotlin.kt
+++ src/boostcourse1/chapter1/HelloKotlin.kt
@@ -3,8 +3,8 @@ package boostcourse1.chapter1
 fun main(args: Array<String>) {
    println("Hello Kotlin!")
 
-   // main에서 매개변수 사용
-   println("args[0] = ${args[0]}")
-   println("args[1] = ${args[1]}")
-   println("args[2] = ${args[2]}")
+   // 매개 변수 없을 시 ArrayIndexOutOfBoundsException 에러 발생
+   println("args[0] = ${args[0]}") //명령행 첫번째 인자
+   println("args[1] = ${args[1]}") //명령행 두번째 인자
+   println("args[2] = ${args[2]}") //명령행 세번째 인자
 }
