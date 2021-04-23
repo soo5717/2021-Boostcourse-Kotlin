888d20590ce649ee1c121f111ec9f226f6410663
soo5717 <koo5717@naver.com>
2021-04-23 17:04:16 +0900

    코틀린 프로그래밍 기본2 - 4단원

---
+++ src/boostcourse2/chapter5/ArrayList.kt
@@ -0,0 +1,11 @@
+package boostcourse2.chapter5
+
+import java.util.*
+
+fun main() {
+// 가변형 List를 생성하고 자바의 ArrayList로 반환
+    val stringList: ArrayList<String> = arrayListOf<String>("Hello", "Kotlin", "Wow")
+    stringList.add("Java") // 추가
+    stringList.remove("Hello") // 삭제
+    println(stringList)
+}
---
+++ src/boostcourse2/chapter5/HashSet.kt
@@ -0,0 +1,8 @@
+package boostcourse2.chapter5
+
+fun main() {
+    val intsHashSet: HashSet<Int> = hashSetOf(6, 3, 4, 7) // 불변성 기능이 없음
+    intsHashSet.add(5) // 추가
+    intsHashSet.remove(6) // 삭제
+    println(intsHashSet)
+}
