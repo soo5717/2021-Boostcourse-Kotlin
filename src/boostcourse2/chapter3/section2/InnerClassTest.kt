aeacb01507e13615bcba72ac8a17ded7dd9eaf8b
soo5717 <koo5717@naver.com>
2021-05-08 23:57:45 +0900

    코틀린 프로그래밍 기본2 - 챕터3

--- src/boostcourse2/chapter3/section2/InnerClassTest.kt
+++ src/boostcourse2/chapter3/section2/InnerClassTest.kt
@@ -1,7 +1,8 @@
 package boostcourse2.chapter3.section2
 
 class Smartphone(val model: String) {
-    private val cpu = "Exynos"
+    private val cpu = "Exynos" // 내부 클래스는 private도 접근 가능
+
     inner class ExternalStorage(val size: Int) {
         fun getInfo() = "${model}: Installed on $cpu with ${size}Gb" // 바깥 클래스의 프로퍼티 접근
     }
--- src/boostcourse2/chapter3/section2/NestedClassTest.kt
+++ src/boostcourse2/chapter3/section2/NestedClassTest.kt
@@ -2,21 +2,36 @@ package boostcourse2.chapter3.section2
 
 class Outer {
     val ov = 5
+    // 자바에서 static 클래스와 같음.
     class Nested {
         val nv = 10
         fun greeting() = "[Nested] Hello ! $nv" // 외부의 ov에는 접근 불가
+
+        // companion object로 선언한 것은 접근 가능
+        fun accessOuter() {
+            println(country)
+            getSomething()
+        }
     }
     fun outside() {
         val msg = Nested().greeting() // Outer 객체 생성 없이 중첩 클래스의 메서드 접근
         println("[Outer]: $msg, ${Nested().nv}") // 중첩 클래스의 프로퍼티 접근
     }
+    companion object {
+        const val country = "Korea"
+        fun getSomething() {
+            return println("Get something")
+        }
+    }
 }
 
 fun main() {
-// static 처럼 Outer의 객체 생성 없이 Nested객체를 생성 사용할 수 있음
+    // static 처럼 Outer의 객체 생성 없이 Nested객체를 생성 사용할 수 있음
     val output = Outer.Nested().greeting()
     println(output)
-// Outer.outside() // 에러! Outer 객체 생성 필요
+
+    // Outer.outside() // 에러! Outer 객체 생성 필요
     val outer = Outer()
     outer.outside()
+    Outer.Nested().accessOuter()
 }
